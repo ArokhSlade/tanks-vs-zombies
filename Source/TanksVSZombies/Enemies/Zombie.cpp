5f5097405ad33cb0548a86e76a07b040c4153271
Arokh Slade <arokhslade@gmail.com>
2025-11-16 18:55:49 +0100

    add protected fields to Zombie

--- Source/TanksVSZombies/Enemies/Zombie.cpp
+++ Source/TanksVSZombies/Enemies/Zombie.cpp
@@ -9,6 +9,17 @@ AZombie::AZombie()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+
+	Health = 100.f;
+	SightDistance = 500.f;
+	SightAngle = 60.f;
+	YawSpeed = 90.f;
+	WalkSpeed = 25.f;
+	RunSpeed = 45.f;
+	AttackDistance = 100.f; // center-to-center
+	AttackAngle = 30.f;
+	AttackCooldown = 1.f;
 }
 
 // Called when the game starts or when spawned
--- Source/TanksVSZombies/Enemies/Zombie.h
+++ Source/TanksVSZombies/Enemies/Zombie.h
@@ -25,5 +25,50 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	/** Camera effet, if any, to play when the player is hit by this Zombie. */
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UCameraShakeBase> HitShake;
 
+	/** Current health value. Might be fun to have different values for different attack types, e.g. fire.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float Health;
+	
+	/** Sight distance (when no target is present)*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float SightDistance;
+	
+	/** Sight half-angle in degrees (when no target is present) */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float SightAngle;
+	//float SightAngleCos; // cached value to save conversions
+	
+	/** Max yaw speed per second in degrees*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float YawSpeed;
+	
+	/** Speed when wandering around, or when acing away from target.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float WalkSpeed;
+	
+	/** Speed when rushing toward target.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float RunSpeed;
+	
+	/** .*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackDistance;
+	
+	/** Half-angle (in degrees) for our attack. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackAngle;
+	
+	/** Minimum time between attacks.*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Zombie", meta = (ClampMin = "0.0"))
+	float AttackCooldown;
+
+	/** Game time, in seconds, when the Zombie will be allowed to attack again. */
+	UPROPERTY(Transient, VisibleInstanceOnly, BlueprintReadWrite, Category = "Zombie")
+	float AttackAvailableTime;
 };
